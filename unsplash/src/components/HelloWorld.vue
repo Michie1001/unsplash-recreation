d48d1ec6def7b2b0632b12dc9efbf8e67a161e3c
Michie1001 <michieugwu@gmail.com>
2020-12-26 23:09:01 +0100

    Created Vue app

---
+++ unsplash/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <header class="header">
+      <input type="" name="" placeholder="Search for photo">
+    </header>
+    <div class="content">
+      <div class="gridArea"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.header{
+  background-color: #dce2e9;
+}
+</style>
